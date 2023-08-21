f560e99e742be36050360a240813dd6ced405aaa
CarbonicAcidWaterPolo <132192878+CarbonicAcidWaterPolo@users.noreply.github.com>
2023-08-21 10:01:57 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,195 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main extends JPanel implements KeyListener {
+    public static final int CELL_SIZE = 20;
+    public static int height = 400;
+    public static int width = 400;
+    public  static  int row = height/CELL_SIZE;
+    public  static  int column = width / CELL_SIZE;
+    private Snake snake;
+    private Fruit fruit;
+    private Timer t;
+    private int speed = 100;
+    private static String direction;
+    private boolean allowKeyPress;
+    private int score;
+    private int highest_score;
+    String desktop = System.getProperty("user.home") + "desktop/";
+    String myFile = desktop + "filename.txt";
+
+    public Main(){
+        read_highest_score();
+        reset();
+        addKeyListener(this);
+    }
+    private void setTimer(){
+        t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                repaint();
+            }
+        }, 0,speed);
+    }
+    private void reset(){
+        score = 0;
+        if (snake != null){
+            snake.getSnakeBody().clear();
+        }
+        allowKeyPress = true;
+        direction = "Right";
+        snake = new Snake();
+        fruit = new Fruit();
+        setTimer();
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        //check if the snake bites itself.
+        ArrayList<Node> snake_body = snake.getSnakeBody();
+        Node head = snake_body.get(0);
+        for ( int i=1 ; i < snake_body.size() ; i++){
+            if ( snake_body.get(i).x == head.x && snake_body.get(i).y == head.y){
+                allowKeyPress = false;
+                t.cancel();
+                t.purge();
+                int response = JOptionPane.showOptionDialog(this,"Game over!+"+" Your score is "+ score + ". The highest score is " + highest_score +". Would you like to start over?","Game over!",JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE,null,null,JOptionPane.YES_OPTION);
+                write_a_file(score);
+                switch (response){
+                    case JOptionPane.CLOSED_OPTION:
+                        System.exit(0);
+                        break;
+                    case JOptionPane.NO_OPTION:
+                        System.exit(0);
+                        break;
+                    case JOptionPane.YES_OPTION:
+                        reset();
+                        return;
+                }
+            }
+        }
+        // Draw a black background
+        g.fillRect(0,0,width,height);
+        snake.drawSnake(g);
+        fruit.drawFruit(g);
+        //Remove snake tail & put it in head.
+        int snakeX = snake.getSnakeBody().get(0).x;
+        int snakeY = snake.getSnakeBody().get(0).y;
+        //right, x+=cell size
+        //left, x-=cell size
+        //up, y-=cell size
+        //down, y+=cell size
+
+        if (direction.equals("Left")){
+            snakeX -= CELL_SIZE;
+        } else if (direction.equals("Right")) {
+            snakeX += CELL_SIZE;
+        }else if (direction.equals("Up")){
+            snakeY -= CELL_SIZE;
+        } else if (direction.equals("Down")) {
+            snakeY += CELL_SIZE;
+        }
+        System.out.println(snakeX + ", " + snakeY);
+
+        Node newHead = new Node(snakeX,snakeY);
+
+        //check if the snake eats the fruit.
+        if (snake.getSnakeBody().get(0).x == fruit.getX() && snake.getSnakeBody().get(0).y == fruit.getY()){
+            fruit.drawFruit(g);
+            fruit.setNewLocation(snake);
+            score++;
+        } else {
+            snake.getSnakeBody().remove(snake.getSnakeBody().size() -1 );
+        }
+        snake.getSnakeBody().add(0, newHead);
+        allowKeyPress = true;
+        requestFocusInWindow();
+    }
+    @Override
+    public Dimension getPreferredSize(){
+        return new Dimension(width,height);
+    }
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Snake Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new Main());
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (allowKeyPress){
+            if (e.getKeyCode() == 65 && !direction.equals("Right")){
+                direction = "Left";
+            } else if (e.getKeyCode() == 87 && !direction.equals("Down")) {
+                direction = "Up";
+            } else if (e.getKeyCode() == 68 && !direction.equals("Left")) {
+                direction = "Right";
+            } else if (e.getKeyCode() == 83 && !direction.equals("Up")) {
+                direction = "Down";
+            }
+            allowKeyPress = false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+    public void read_highest_score(){
+        try {
+            File myObject = new File(myFile);
+            Scanner myReader = new Scanner(myObject);
+            highest_score = myReader.nextInt();
+            myReader.close();
+        } catch (FileNotFoundException e){
+            highest_score = 0;
+            try {
+                File myObject = new File(myFile);
+                if (myObject.createNewFile()){
+                    System.out.println("File created:" + myObject.getName());
+                }
+                FileWriter myWriter = new FileWriter(myObject.getName());
+                myWriter.write(""+0);
+                myWriter.close();
+            }catch (IOException err){
+                System.out.println("An error occurred.");
+                err.printStackTrace();
+            }
+        }
+    }
+    public void write_a_file(int score){
+        try{
+            FileWriter myWriter = new FileWriter(myFile);
+            if ( score > highest_score ) {
+                System.out.println("rewriting score...");
+                myWriter.write(""+score);
+                highest_score = score;
+                myWriter.close();
+            } else {
+                myWriter.write(""+highest_score);
+            }
+        } catch (IOException err){
+            err.printStackTrace();
+        }
+    }
+}
